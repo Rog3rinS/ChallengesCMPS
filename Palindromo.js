054e95aa63a316f98b32587e700f26f488707589
Rog3rinS <rogeriogoioficial@gmail.com>
2025-03-17 10:49:34 -0300

    feat: implementar verificação de palíndromo com inversão manual de string

---
+++ Palindromo.js
@@ -0,0 +1,24 @@
+const { log } = require("console");
+const readline = require("readline");
+
+const leitor = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+let isPalindromo = "";
+
+leitor.question("Palindromo: ", (palindromo) => {
+	palindromo = palindromo.split(" ").join("")
+	for (let i = palindromo.length - 1; i >= 0; i--) {
+		isPalindromo += palindromo[i]
+	}
+	if (isPalindromo == palindromo) {
+		console.log("E um palindromo")
+	}
+	else {
+		console.log("Nao e um palindromo")
+	}
+	leitor.close;
+});
+
